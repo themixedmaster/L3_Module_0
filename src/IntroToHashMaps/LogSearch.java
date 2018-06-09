876e2fe38253dfcdd1ebd558223d8b6d329ac36c
Gavin Morris <themixedmaster@gmail.com>
2018-06-09 14:27:27 -0700

    Week 112
    We got nothing done

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -20,11 +20,12 @@ public static void main(String[] args) {
 	
    JButton b = new JButton("Add Prisoner");
    JButton bb = new JButton("Cell Check");
-   JButton bbb = new JButton();
+   JButton bbb = new JButton("Prison Search");
+   JButton bbbb = new JButton("Free Prisoner");
    HashMap<Integer,String> prison = new HashMap<Integer,String>();
    void m (){
 		 // Create a GUI with three buttons. 
-		   JFrame box = new JFrame();
+		   JFrame box = new JFrame("The Prison of unescapable death and unimaginable horror and invincible prison cells that only a god could escape from because of how impossible it is to escape");
 		   JPanel pannel = new JPanel();
 		   box.add(pannel);
 		   box.setVisible(true);
@@ -32,6 +33,7 @@ public static void main(String[] args) {
 		   pannel.add(b);
 		   pannel.add(bb);
 		   pannel.add(bbb);
+		   pannel.add(bbbb);
 		 // Button 1: Add Entry
 		   b.addActionListener(this);
 		   // 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
@@ -44,19 +46,12 @@ public static void main(String[] args) {
 			 // 				If that ID exists, display that name to the user.
 			 // 				Otherwise, tell the user that that entry does not exist.
 			 // 
-			 // Button 3: View List
-			 // 				When this button is clicked, display the entire list in a message dialog in the following format:
-			 // 				ID: 123  Name: Harry Howard
-			 // 				ID: 245  Name: Polly Powers
-			 // 				ID: 433  Name: Oliver Ortega
-			 // 				etc...
 			 // 
+		   bbb.addActionListener(this);
 			 // When this is complete, add a fourth button to your window.
 			 // Button 4: Remove Entry
-			 // 				When this button is clicked, prompt the user to enter an ID using an input dialog.
-			 // 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
-			 // 				is not in the list. 
-			 //
+		   bbbb.addActionListener(this);
+			 
    }
 @Override
 public void actionPerformed(ActionEvent e) {
@@ -65,8 +60,8 @@ public void actionPerformed(ActionEvent e) {
 		int iD = Integer.parseInt(qwertyuiop);
 // 				After an ID is entered, use another input dialog to ask the user to enter a name.
 		qwertyuiop = JOptionPane.showInputDialog("What is this prisoner's name?");
-		if(qwertyuiop == ("Danny Devito")){
-			prison.put(iD,"missing. He seems to have dug through the floor using his god-like strength. He left a lot of trash behind.");
+		if(qwertyuiop.equals("Danny Devito")){
+			prison.put(iD,"MISSING! He seems to have dug through the floor using his god-like strength. He left a lot of trash behind though.");
 		}else{
 			prison.put(iD,qwertyuiop);
 		}
@@ -75,13 +70,38 @@ public void actionPerformed(ActionEvent e) {
 //			When this button is clicked, use an input dialog to ask the user to enter an ID number.
 		String qwertyuiop = JOptionPane.showInputDialog("Insert the Id of the prisoner who you want to find."); 
 	 // 				If that ID exists, display that name to the user.
-		if(qwertyuiop == null){
-			JOptionPane.showMessageDialog(null,"No-one's there.");
-		}else{
+		int qwertyuio = Integer.parseInt(qwertyuiop);
+		if(prison.containsKey(qwertyuio)){
 			JOptionPane.showMessageDialog(null, "The prisoner in cell " + qwertyuiop + " is " + prison.get(Integer.parseInt(qwertyuiop)));
-		}
+		}else{
+			JOptionPane.showMessageDialog(null,"No-one's there.");
+			}
 	 // 				Otherwise, tell the user that that entry does not exist.
 	 // 
+	}else if(e.getSource() == bbb){
+		 // Button 3: View List
+		 // 				When this button is clicked, display the entire list in a message dialog in the following format:
+		for(int i:prison.keySet()){
+			JOptionPane.showMessageDialog(null,"Cell: " + i + "  |  PRISONER: " + prison.get(i));
+		}			
+		
+	}else if(e.getSource() == bbbb){
+//		When this button is clicked, prompt the user to enter an ID using an input dialog.
+		String qwertyuiop = JOptionPane.showInputDialog("Insert the ID of the prisoner you want to free:");
+		int qwertyuio = Integer.parseInt(qwertyuiop);
+ // 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
+		if(prison.containsKey(qwertyuio)){
+			if(prison.get(qwertyuio).equals("MISSING! He seems to have dug through the floor using his god-like strength. He left a lot of trash behind though.")){
+				JOptionPane.showMessageDialog(null, "No-ones there. The prisoner here seems to have dug through the floor using his god-like strength. He left a lot of trash behind though.");
+			}else{
+				JOptionPane.showMessageDialog(null, prison.get(qwertyuio) + " has been freed!");
+				prison.remove(qwertyuio);
+			}
+		}else{
+			JOptionPane.showMessageDialog(null, "No-one's there");
+		}
+ // 				is not in the list. 
+ //
 	}
 	
 	// TODO Auto-generated method stub
